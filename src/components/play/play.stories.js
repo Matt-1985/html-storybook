faf162b61c3a2f9b752c9194ec07cbb1bf008853
Matt-1985 <blatsche@web.de>
2020-10-05 18:17:29 +0200

    Add button

---
+++ src/components/play/play.stories.js
@@ -0,0 +1,8 @@
+import "./play.css";
+import buttonText from "./button-text.html";
+import buttonLogo from "./button-logo.html";
+
+export default { title: "Components/play" };
+
+export const withText = () => buttonText;
+export const withLogo = () => buttonLogo;
