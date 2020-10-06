6d4acd9d215647e3f400fbf38eb24d5d03cdfa54
Matt-1985 <blatsche@web.de>
2020-10-06 18:09:48 +0200

    Add List item not final

--- src/components/share/share.stories.js
+++ src/components/share/share.stories.js
@@ -1,8 +1,7 @@
 import "./share.css";
-import shareText from "./share-text.html";
+
 import shareLogo from "./share-logo.html";
 
 export default { title: "Components/Share" };
 
-export const withText = () => shareText;
 export const withLogo = () => shareLogo;
