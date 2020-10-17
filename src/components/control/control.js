66c8dea8b6b837516cdbe7416d21a4ff2816e53f
Matt-1985 <blatsche@web.de>
2020-10-17 16:16:40 +0200

    Add changes

--- src/components/control/control.js
+++ src/components/control/control.js
@@ -1,9 +1,8 @@
 import { createElement } from "../../utils/elements";
 import "./control.css";
 
-
-
-{/* <div>
+{
+  /* <div>
   <div>
     <button>
       <img src="" alt="" />
@@ -23,25 +22,25 @@ import "./control.css";
     <img src="" alt="" />
     <span></span>
   </div>
-</div> */}
-
-export const createTrackElement = (track) => {
-
-const controlElement = createElement("div", {
-className: "container",
-children: [
-    createElement("button", {
-        className: "control__btn",
-    }),
-    createElement("img", {
-        className: "control__img"
-        src: control.imgSrc
-        alt: "Backwards"
-    }),
-
-],
-
-}),
-
-
+</div> */
 }
+
+// export const createTrackElement = (track) => {
+
+// const controlElement = createElement("div", {
+// className: "container",
+// children: [
+//     createElement("button", {
+//         className: "control__btn",
+//     }),
+//     createElement("img", {
+//         className: "control__img"
+//         src: control.imgSrc
+//         alt: "Backwards"
+//     }),
+
+// ],
+
+// }),
+
+// }
